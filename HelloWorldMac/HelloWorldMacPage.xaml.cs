5f6b606fc6bc47faa6ae5fe60dcbf16d996fc956
jrstrayhorn <jrstrayhorn@gmail.com>
2018-03-20 07:58:36 -0400

    Initial check-in of module HelloWorldMac

---
+++ HelloWorldMac/HelloWorldMacPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace HelloWorldMac
+{
+    public partial class HelloWorldMacPage : ContentPage
+    {
+        public HelloWorldMacPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
